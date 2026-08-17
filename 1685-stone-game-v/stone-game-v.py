efd3256c677cf6325a01d4107da18e85a341fa4f
Vishalini Malekar <vishalinim77@gmail.com>
2026-08-17 22:28:45 +0530

    Time: 2153 ms (58.04%) | Memory: 53.8 MB (37.25%) - LeetSync

---
+++ 1685-stone-game-v/stone-game-v.py
@@ -0,0 +1,45 @@
+from functools import cache
+
+class Solution:
+    def stoneGameV(self, stoneValue):
+        n = len(stoneValue)
+
+        prefix = [0] * (n + 1)
+
+        for i in range(n):
+            prefix[i + 1] = prefix[i] + stoneValue[i]
+
+        @cache
+        def dfs(l, r):
+            if l >= r:
+                return 0
+
+            ans = 0
+            left_sum = 0
+            right_sum = prefix[r + 1] - prefix[l]
+
+            for k in range(l, r):
+                left_sum += stoneValue[k]
+                right_sum -= stoneValue[k]
+
+                if left_sum < right_sum:
+                    if ans >= 2 * left_sum:
+                        continue
+                    ans = max(ans, left_sum + dfs(l, k))
+
+                elif left_sum > right_sum:
+                    if ans >= 2 * right_sum:
+                        break
+                    ans = max(ans, right_sum + dfs(k + 1, r))
+
+                else:
+                    ans = max(
+                        ans,
+                        left_sum + dfs(l, k),
+                        right_sum + dfs(k + 1, r)
+                    )
+
+            return ans
+
+        return dfs(0, n - 1)
+        
